b1ff4c684729cd44dc0596c46735250d5e6dc840
joelbenjaminboer <joelbenjaminboer@gmail.com>
2025-01-22 15:38:20 +0100

    added metrics

---
+++ metrics/Root_mean_squared_error.py
@@ -0,0 +1,27 @@
+from sklearn.metrics import mean_squared_error
+
+
+class RootMeanSquaredError:
+    """
+    RootMeanSquaredError
+
+    This class is a wrapper for the mean_squared_error function from sklearn.
+    """
+    def __init__(self):
+        """
+        initialize the metric with the mean_squared_error function from sklearn
+        """
+        self.metric = mean_squared_error
+
+    def calculate(self, y_true, y_pred):
+        """
+        calculate the mean squared error for the true and predicted values
+
+        Parameters
+        ----------
+        y_true : array-like
+            The true target values.
+        y_pred : array-like
+            The predicted target values.
+        """
+        return self.metric(y_true, y_pred, squared=False)
